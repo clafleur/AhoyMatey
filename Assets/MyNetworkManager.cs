6eed69d9dd84037a4cbc8f7f169fc1b791722ac7
clafleur <clayton@texsstudio.com>
2016-09-26 14:43:33 -0700

    Created a client and tested Network Movement
    Created a build to use a my client.
    Set up Cube with a Network Transform to test movement over the network.
    Updated the MyHub Script to be a MyNetworkManager script.
    Set the MyNetworkManager script to Inherit from NetworkManager.
    Did three overrides to log out information about the client and the host
    machine

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log(Time.timeSinceLevelLoad +" Starting Host at");
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Host started at");
+    }
+
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " Client started at");
+        InvokeRepeating("PrintDots", 0f, 1f);
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log(Time.timeSinceLevelLoad + " client is connected to IP: " + conn.address);
+        CancelInvoke();
+    }
+
+    void PrintDots() {
+        Debug.Log(".");
+    }
+}
