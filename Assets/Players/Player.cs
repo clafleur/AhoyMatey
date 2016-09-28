9c0bd84a744e94442edb2c762c26c5cad06e7b98
clafleur <clayton@texsstudio.com>
2016-09-28 09:40:14 -0700

    Created a script for the player to track movement
    Created a script for movement so we can see the player move on two
    diffrent devices.

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector3 inputValue;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+        inputValue.y = 0.0f;
+        inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+        transform.Translate(inputValue);
+    }
+}
